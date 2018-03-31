c9de4d1807c31fba441f6706a3e6023a919cdf9a
Jazilch <jzilch@hubspot.com>
2018-03-31 15:49:34 -0400

    Firebase Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,15 @@
 import * as firebase from 'firebase';
+import keys from '../config/prod';
 
+console.log(keys.apiKey)
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIRBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: keys.apiKey,
+  authDomain: keys.authDomain,
+  databaseURL: keys.databaseURL,
+  projectId: keys.projectId,
+  storageBucket: keys.storageBucket,
+  messagingSenderId: keys.messagingSenderId
 };
 firebase.initializeApp(config);
 
