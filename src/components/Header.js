3cac696dbed810f07cc3cf878f5f8ed88cac8e1b
Jazilch <jzilch@hubspot.com>
2018-04-01 15:58:24 -0400

    Firebase Auth & Login Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-const Header = () => (
-	<header className="header">
-		<div className="content-container">
-		  <div className="header__content">
-		    <h1>Expensify</h1>
-					<nav>
-						<NavLink to="/" activeClassName="is-active" exact>Home</NavLink>
-						<NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
-						<NavLink to="/help" activeClassName="is-active">Help</NavLink>
-					</nav>
-				</div>
-			</div>
-	</header>
+export const Header = ({ startLogout }) => (
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dashboard">
+          <h1>Expensify</h1>
+        </Link>
+        <button className="button button--link" onClick={startLogout}>Logout</button>
+      </div>
+    </div>
+  </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { startSetExpenses } from './actions/expenses';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import './firebase/firebase';
+import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
+import getVisibleExpenses from './selectors/expenses';
+import { firebase } from './firebase/firebase';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import LoadingPage from './components/LoadingPage';
@@ -19,10 +21,30 @@ const jsx = (
 		<AppRouter />
 	</Provider>
 );
+let hasRendered = false;
+const renderApp = () => {
+	if(!hasRendered) {
+		ReactDOM.render(jsx, document.getElementById('root'));
+		registerServiceWorker();
+		hasRendered = true;
+	}
+};
 
 ReactDOM.render(<LoadingPage />, document.getElementById('root'));
 
-store.dispatch(startSetExpenses()).then(() => {
-	ReactDOM.render(jsx, document.getElementById('root'));
-	registerServiceWorker();
+firebase.auth().onAuthStateChanged((user) => {
+	if (user) {
+		store.dispatch(login(user.uid));
+		store.dispatch(startSetExpenses()).then(() => {
+			renderApp();
+			registerServiceWorker();
+			if (history.location.pathname === "/") {
+				history.push('/dashboard');
+			}
+		});
+	} else {
+		store.dispatch(logout());
+		renderApp();
+		history.push('/');
+	}
 });
